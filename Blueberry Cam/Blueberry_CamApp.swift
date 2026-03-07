48edf16ce1352f09474e915e8d059e2cf85a51cd
Rishi <rishi_j@icloud.com>
2026-03-07 15:22:29 +0000

    Initial Commit

---
+++ Blueberry Cam/Blueberry_CamApp.swift
@@ -0,0 +1,17 @@
+//
+//  Blueberry_CamApp.swift
+//  Blueberry Cam
+//
+//  Created by Rishi Jansari on 07/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct Blueberry_CamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
