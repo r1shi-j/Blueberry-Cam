a977ce23001f4e0efd1266b8e17fabab9dc50d57
Rishi <rishi_j@icloud.com>
2026-03-15 22:18:54 +0000

    Added future ToDos

--- Blueberry Cam/Blueberry_CamApp.swift
+++ Blueberry Cam/Blueberry_CamApp.swift
@@ -27,3 +27,19 @@ struct Blueberry_CamApp: App {
         .handlesExternalEvents(matching: ["*"])
     }
 }
+
+// MARK: - TODO
+// Localise for all iPhones iOS 26+
+// Improve memory/cpu usage, quicker camera switching
+// Move lens picker to bottom right?
+// Click on any readout -> shows vertical slider right side screen like halide
+/// slider shows the auto value constantly updating, manually changing it turns on manual mode, for ss/iso have left iso right ss?
+// Multiple graphs at once
+/// Hold bottom graph to disappear, hold top graph to disappear
+/// When no graph top, button owl be show to show a graph
+/// To show graph at bottom go in settings?
+// Show proper selfie cameras
+// Tap to focus with focus EV
+// Front and rear photos simultaneously
+// Macro
+// QR codes - shake to open
